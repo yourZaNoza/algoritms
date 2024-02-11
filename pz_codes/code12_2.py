f31e89edd86c3de46ca1d0b8200ed2fac9356a55
yourZaNoza <yourZaNoza@yandex.ru>
2024-02-11 15:29:32 +0300

    another commit

---
+++ pz_codes/code12_2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Вариант 20.
+# 2. Из заданной строки отобразить только символы нижнего регистра. Использовать библиотеку string.
+# Строка 'In PyCharm, you can specify third-party standalone applications and run them as External Tools'.
+from string import ascii_lowercase
+
+string_new = 'In PyCharm, you can specify third-party standalone applications and run them as External Tools'
+str_n = {i for i in string_new if i in ascii_lowercase}
+print('Найдены все уникальные прописные буквы:')
+print(str_n)
