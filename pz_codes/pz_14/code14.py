5e4a389c8019d458206f559fb3a6f4156fb445ca
yourZaNoza <yourZaNoza@yandex.ru>
2024-03-20 13:45:14 +0300

    another commit

--- pz_codes/pz_14/code14.py
+++ pz_codes/pz_14/code14.py
@@ -12,25 +12,19 @@ for i in open('writer.txt', encoding='utf-8'):
     # print(matches)
 print(f'Количество фамилий в файле: {c}')
 
-# old = open('text18-20.txt', encoding='utf-16')
-# text = old.read()
-# lines = text.splitlines()
-# rewrite = re.findall()
-# old.close()
-# new = open(new_writer.txt, 'w')
-# new.writelines(max_line)
-# new.close()
-
-roman_endings = ['', 'а', 'ов']
-prod_endings = ['ие', 'иев', 'ий', 'ия']
+endings = {
+    '': 'е',
+    'а': 'я',
+    'ов': 'й',
+}
 
 with open('writer.txt', 'r', encoding='utf-8') as f1:
     text = f1.read()
-for re_ending in roman_endings:
-    for pe_ending in prod_endings:
-        pattern = r'\bроман(?=\b|' + re_ending + r')'
-        replacement = 'произведен' + pe_ending
-        text = re.sub(pattern, replacement, text)
+for ending in list(endings.keys()):
+    pattern = rf"\bроман{ending}\b"
+    replacement = 'произведени' + endings[ending]
+    text = re.sub(pattern, replacement, text)
+
 with open('new_writer.txt', 'w', encoding='utf-8') as f2:
     f2.write(text)
 
